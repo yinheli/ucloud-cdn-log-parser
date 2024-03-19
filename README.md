10120b67468d0996711998b5f81742397910d217
yinheli <me@yinheli.com>
2024-03-19 17:20:55 +0800

    feat: support tsv format output

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ucloud-cdn-log-parser
 
-Parse ucloud cdn log to csv format with header, then you can use duckdb / clickhouse local / etc to analyze the log.
+Parse ucloud cdn log to csv/tsv format with/without header, then you can use duckdb / clickhouse local / etc to analyze the log.
 
 ## Install
 
@@ -17,7 +17,11 @@ cargo install ucloud-cdn-log-parser --locked
 ```bash
 # download logs
 
-# parse & convert to parquet
+# parse & convert to csv / parquet
+
+zcat *.gz | \
+  ucloud-cdn-log-parser > log.csv
+
 zcat *.gz | \
   ucloud-cdn-log-parser | \
   duckdb -c "copy (select * from read_csv('/dev/stdin')) to 'log.parquet' (format parquet)"
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,127 @@
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead, Write}, process::exit};
 
+use clap::{command, Parser, ValueEnum};
+use csv::Writer;
 use regex::Regex;
 
-fn main() -> io::Result<()> {
-    let re = Regex::new(r#"(\[.*?\])|(".*?")|(-)|(\S+)"#).unwrap();
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[clap(long, default_value = "true")]
+    header: bool,
+
+    #[clap(short, long, default_value = "csv")]
+    format: Format,
+}
+
+#[derive(Debug, Clone, PartialEq, ValueEnum)]
+enum Format {
+    CSV,
+    TSV,
+}
+
+fn main(){
+    let arg = Args::parse();
+
+    let mut wb = csv::WriterBuilder::new();
+    wb.quote_style(csv::QuoteStyle::NonNumeric);
     
-    let stdin = io::stdin();
-    let mut w = csv::WriterBuilder::new()
-        .delimiter(b',')
-        .quote_style(csv::QuoteStyle::NonNumeric)
-        .terminator(csv::Terminator::CRLF)
-        .from_writer(io::stdout());
+    match arg.format {
+        Format::CSV => {
+            wb.delimiter(b',');
+        }
+        Format::TSV => {
+            wb.delimiter(b'\t');
+        }
+    }
 
+    let w = wb.from_writer(io::stdout());
+    if let Err(e) = parse_and_write(w) {
+        if e.kind() != io::ErrorKind::BrokenPipe {
+            return;
+        }
+        eprintln!("Error: {}", e);
+        exit(1)
+    }
+
+}
+
+fn parse_and_write<W: Write>(mut w: Writer<W>) -> io::Result<()> {
     let headers = vec![
-        "date_time", "client_ip", "request_method_url_protocol", 
-        "hit_status", "response_code", "sent_bytes_incl_header", 
-        "response_delay", "http_host", "http_referer", "http_username", 
-        "origin_way_host", "server_ip", "source_ip", "source_response_code", 
-        "client_request_end", "source_request_end", "user_agent", 
-        "request_processing_start", "frontend_flow", "http_range", 
-        "sent_bytes_excl_header", "file_size", "cache_hit_bytes", 
-        "merged_origin_bytes", "internal_error_code", "end_status",
-        "ufile_received_bytes", "cache_port", "transfer_protocol", "request_host"
+        "date_time",
+        "client_ip",
+        "request_method_url_protocol",
+        "hit_status",
+        "response_code",
+        "sent_bytes_incl_header",
+        "response_delay",
+        "http_host",
+        "http_referer",
+        "http_username",
+        "origin_way_host",
+        "server_ip",
+        "source_ip",
+        "source_response_code",
+        "client_request_end",
+        "source_request_end",
+        "user_agent",
+        "request_processing_start",
+        "frontend_flow",
+        "http_range",
+        "sent_bytes_excl_header",
+        "file_size",
+        "cache_hit_bytes",
+        "merged_origin_bytes",
+        "internal_error_code",
+        "end_status",
+        "ufile_received_bytes",
+        "cache_port",
+        "transfer_protocol",
+        "request_host",
     ];
-
     w.write_record(&headers)?;
+
+    let re = Regex::new(r#"(\[.*?\])|(".*?")|(-)|(\S+)"#).unwrap();
+    let stdin = io::stdin();
 
     for line in stdin.lock().lines() {
         let line = line?;
-
         let mats = re.captures_iter(&line);
-
-        let items = mats.flat_map(|caps| {
-            caps.iter()
-                .skip(1)
-                .filter(|v|v.is_some())
-                .map(|v|{
-                    v.unwrap().as_str()
-                        .trim_matches(|c| c == '"' || c == '[' || c == ']')
-                }).collect::<Vec<_>>()
-        }).collect::<Vec<_>>();
+        let items = mats
+            .flat_map(|caps| {
+                caps.iter()
+                    .skip(1)
+                    .filter(|v| v.is_some())
+                    .map(|it| {
+                        let v = it.unwrap()
+                            .as_str()
+                            .trim_matches(|c|  c == '[' || c == ']')
+                            .replace("\r", "")
+                            .replace("\t", "")
+                            .replace("\n", "")
+                            .replace("NONE/-", ""); // fix NONE/- prevent hints to date_time
+                        if v == "-" {
+                            "".to_string()
+                        } else {
+                            v
+                        }
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
 
         if items.len() != headers.len() {
-            eprintln!("Error: require size: {}, get size: {}\n\n  {}\n\n  {}", headers.len(), items.len(), items.join(","), line);
+            eprintln!(
+                "Error: require size: {}, get size: {}\n\n  {}\n\n  {}",
+                headers.len(),
+                items.len(),
+                items.join(","),
+                line
+            );
             continue;
         }
 
         w.write_record(&items)?;
     }
-
     w.flush()
 }
